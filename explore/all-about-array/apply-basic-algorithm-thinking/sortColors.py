615b111756ffc850a81ed0528765eebdadc63594
Lingmoumou <hyuan.feng@gmail.com>
2018-11-22 23:54:53 +0800

    first commit folder

---
+++ explore/all-about-array/apply-basic-algorithm-thinking/sortColors.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums):
+        """
+        给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+        此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+
+
+def main():
+    s=Solution()
+    s.sortColors()
+
+if __name__ == "__main__":
+    main()
+        
